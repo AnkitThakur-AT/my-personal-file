ea113416f4cb9c02d0fdd1e42996e565a1cbcf14
AnkitThakur-AT <ankitthakurat452@gmail.com>
2025-03-18 11:46:58 +0530

    file added

---
+++ script.js
@@ -0,0 +1,16 @@
+var scrollBtn = document.getElementById("scrollUp");
+
+        // Show the button when user scrolls down 200px
+        window.onscroll = function () {
+            if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+                scrollBtn.style.display = "block";
+            } else {
+                scrollBtn.style.display = "none";
+            }
+        };
+
+        // Scroll to the top smoothly when button is clicked
+        scrollBtn.addEventListener("click", function (event) {
+            event.preventDefault(); // Prevent default link action
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        });
